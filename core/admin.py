17ef5ad6fa46599f782f292647a5b05c2c193bde
ibrahemlolo <ibrahemlolo.sy@gmail.com>
2024-05-10 14:43:25 +0300

    end of authentication

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,16 @@
+from atexit import register
 from store.models import Product
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
+from .models import User
 
+
+@admin.register(User)
+class UserAdmin (BaseUserAdmin):
+    pass
 
 class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
